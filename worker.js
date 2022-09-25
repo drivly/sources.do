10a99af40eafd899cca1bd5c2546537df4f65859
Nathan Clevenger <nateclev@gmail.com>
2022-09-25 08:13:50 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -19,7 +19,7 @@ export const api = {
 export default {
   fetch: async (req, env) => {
     const { user, origin, requestId, method, body, time, pathname, pathSegments, pathOptions, url, query } = await env.CTX.fetch(req).then(res => res.json())
-    if (pathname == '/subscribe' && !user.profile) return Response.redirect(origin + '/login')
+    if (pathname != '/api' && !user.profile) return Response.redirect(origin + '/login')
 
     return new Response(JSON.stringify({ api, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
   },
