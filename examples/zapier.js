1fc587cc3b446af10f0d3c978b02b594d5c7fc6e
Nathan Clevenger <nateclev@gmail.com>
2022-11-26 07:13:16 -0600

    Update zapier.js

--- examples/zapier.js
+++ examples/zapier.js
@@ -1,3 +1,5 @@
+// curl -H "Host: zapier.com" -H "accept: application/json" --compressed "https://zapier.com/api/v4/implementations-meta/public/"
+
 export default {
-  
+  'apps': () => fetch('https://zapier.com/api/v4/implementations-meta/public/', { headers: { accept: 'application/json' }})
 }
